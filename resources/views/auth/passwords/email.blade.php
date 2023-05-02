3efc39e2bf2836da36a08a2a8193fffed74956fb
tjsk002 <tjsk002@naver.com>
2023-05-02 19:12:35 +0900

    [feature] email

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -14,7 +14,6 @@
                     @endif
                     <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
                         {{ csrf_field() }}
-
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
