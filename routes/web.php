38f0a6a577aed44c2caf6fe086539558478026b7
tjsk002 <tjsk002@naver.com>
2023-05-25 21:31:23 +0900

    [feature] delete

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,6 @@ EOT;
 
 Event::listen('article.created', function ($article) {
     var_dump('이벤트를 받았습니다. 받은 데이터(상태)는 다음과 같습니다.');
-    var_dump($article->toArray());
 });
 Route::get('/', 'Home\Home2Controller@home')
 //    return view('layouts.header')
