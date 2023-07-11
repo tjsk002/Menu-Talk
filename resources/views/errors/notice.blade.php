412c152f18918964b2b31b4ab9e7890267cc94d8
tjsk002 <tjsk002@naver.com>
2023-07-11 22:34:38 +0900

    [feature] notice

--- resources/views/errors/notice.blade.php
+++ resources/views/errors/notice.blade.php
@@ -4,7 +4,6 @@
 <meta charset="utf-8">
     <title>{{$title}}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-
 </head>
 <body>
 <h1>{{$title}}</h1>
