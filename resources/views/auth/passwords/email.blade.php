5cc30cf3f3a2102198f7aea4136ef71c424b1cf8
tjsk002 <tjsk002@naver.com>
2023-08-11 18:32:58 +0900

    [feature] email

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -27,7 +27,6 @@
                                 @endif
                             </div>
                         </div>
-
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
