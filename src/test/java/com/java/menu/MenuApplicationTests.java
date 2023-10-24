bc6066f25974d69fdd3fccc918e1c07c2cc422e4
tjsk002 <tjsk002@naver.com>
2023-10-24 21:09:53 +0900

    Initial commit

---
+++ src/test/java/com/java/menu/MenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
