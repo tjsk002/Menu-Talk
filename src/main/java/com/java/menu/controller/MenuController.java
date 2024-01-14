3d4d357382b756200c666f407f28c32f693f3df0
tjsk002 <tjsk002@naver.com>
2024-01-14 17:19:45 +0900

    menu write add

--- src/main/java/com/java/menu/controller/MenuController.java
+++ src/main/java/com/java/menu/controller/MenuController.java
@@ -2,14 +2,21 @@ package com.java.menu.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MenuController {
-    @GetMapping("/")
-    @ResponseBody
-    public String main(){
 
-        return "Hello world";
-    }
+    // view file - //localhost:10040/menu/write
+    @GetMapping("/menu/write")
+   public String MenuWriteForm(){
+        System.out.println("90900");
+       return "menu/menu-write";
+   }
+
+   @PostMapping("/menu/write-process")
+   public String MenuWriteProcess(String title, String content){
+       System.out.println(title + content);
+       return "";
+   }
 }
---
+++ src/main/java/com/java/menu/entity/MenuItems.java
@@ -0,0 +1,20 @@
+package com.java.menu.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+// 테이블 이름이랑 동일하게 만드는걸 추천
+@Entity
+public class MenuItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // jpa가 읽음
+    private Integer id;
+    private String title;
+    private String sub_title;
+    private String content;
+    private Float price;
+    private String tag;
+}
