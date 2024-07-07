c91386fce4655f635ef874b9fd7732a680d30864
tjsk002 <tjsk002@naver.com>
2024-07-07 20:57:18 +0900

    docker 세팅수정

--- config/database.php
+++ config/database.php
@@ -40,26 +40,26 @@ return [
             'foreign_key_constraints' => true,
         ],
 
-        'mysql' => [
-            'sticky'    => true,
-            'driver' => 'mysql',
-//            'host' => env('DB_HOST', '127.0.0.1'),
-//            'port' => env('DB_PORT', '3306'),
-            'host' => 'b-flow-test.clfomwrfhqa7.ap-northeast-2.rds.amazonaws.com',
-            'port' => 3306,
-//            'database' => env('DB_DATABASE', 'forge'),
-//            'username' => env('DB_USERNAME', 'forge'),
-//            'password' => env('DB_PASSWORD', ''),
-            'database'  => 'crudphp',
-            'username'  => 'develover',
-            'password'  => 'brich0707',
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
+//        'mysql' => [
+//            'sticky'    => true,
+//            'driver' => 'mysql',
+////            'host' => env('DB_HOST', '127.0.0.1'),
+////            'port' => env('DB_PORT', '3306'),
+//            'host' => 'b-flow-test.clfomwrfhqa7.ap-northeast-2.rds.amazonaws.com',
+//            'port' => 3306,
+////            'database' => env('DB_DATABASE', 'forge'),
+////            'username' => env('DB_USERNAME', 'forge'),
+////            'password' => env('DB_PASSWORD', ''),
+//            'database'  => 'menu-talk-php',
+//            'username'  => 'develover',
+//            'password'  => 'brich0707',
+//            'unix_socket' => env('DB_SOCKET', ''),
+//            'charset' => 'utf8mb4',
+//            'collation' => 'utf8mb4_unicode_ci',
+//            'prefix' => '',
+//            'strict' => true,
+//            'engine' => null,
+//        ],
 
 //        'mysql_new' => [
 //            'read' => [
@@ -87,29 +87,29 @@ return [
 //        ],
 
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
-
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-        ],
+//        'pgsql' => [
+//            'driver' => 'pgsql',
+//            'host' => env('DB_HOST', '127.0.0.1'),
+//            'port' => env('DB_PORT', '5432'),
+//            'database' => env('DB_DATABASE', 'forge'),
+//            'username' => env('DB_USERNAME', 'forge'),
+//            'password' => env('DB_PASSWORD', ''),
+//            'charset' => 'utf8',
+//            'prefix' => '',
+//            'schema' => 'public',
+//            'sslmode' => 'prefer',
+//        ],
+//
+//        'sqlsrv' => [
+//            'driver' => 'sqlsrv',
+//            'host' => env('DB_HOST', 'localhost'),
+//            'port' => env('DB_PORT', '1433'),
+//            'database' => env('DB_DATABASE', 'forge'),
+//            'username' => env('DB_USERNAME', 'forge'),
+//            'password' => env('DB_PASSWORD', ''),
+//            'charset' => 'utf8',
+//            'prefix' => '',
+//        ],
 
     ],
 
@@ -137,17 +137,17 @@ return [
     |
     */
 
-    'redis' => [
-
-        'client' => 'predis',
-
-        'default' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => 0,
-        ],
-
-    ],
+//    'redis' => [
+//
+//        'client' => 'predis',
+//
+//        'default' => [
+//            'host' => env('REDIS_HOST', '127.0.0.1'),
+//            'password' => env('REDIS_PASSWORD', null),
+//            'port' => env('REDIS_PORT', 6379),
+//            'database' => 0,
+//        ],
+//
+//    ],
 
 ];
