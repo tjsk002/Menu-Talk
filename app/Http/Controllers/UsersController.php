4b79981e9e36e34350938604a6c11f085cb950e0
tjsk002 <tjsk002@naver.com>
2025-01-11 23:40:44 +0900

    user store add

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Domains\Utils\Traits\ControllerTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UsersController extends Controller
 {
@@ -27,21 +29,32 @@ class UsersController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'name'  => 'required|string|max:100', // 회원 이름
-            'phone_number' => 'required|string|max:100', // 회원 연락처
+            'company_name'  => 'required|string|max:50', // 회사 이름
+            'phone_number' => 'required|string|max:50', // 회원 연락처
             'email'      => 'required|string|email|max:255|unique:users', // 회원 이메일
             'password'   => 'required|string|min:6|confirmed',
-            'company_name'  => 'required|string|max:100', // 회사 이름
-            'business_number'  => 'required|string|max:100', // 사업자 번호
-            'company_number'  => 'required|string|max:100', // 회사 연락처
+            'business_number'  => 'required|string|max:50', // 사업자 번호
+            'company_number'  => 'required|string|max:50', // 회사 연락처
         ]);
 
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()->all()], 422);
         }
 
-        // 1. 'users' 테이블에 email 데이터 있는지 확인
-        // 2. 회원 가입 데이터 저장 'users'
-        // 3. 성공 리턴
+        try {
+            $user = User::create([
+                'name' => $request->input('name'),
+                'company_name' => $request->input('company_name'),
+                'phone_number' => $request->input('phone_number'),
+                'email' => $request->input('email'),
+                'password' => Hash::make($request->input('password')),
+                'business_id' => $request->input('business_number'),
+                'company_number' => $request->input('company_number')
+            ]);
+        }catch(Exception $e){
+            return response()->json(['message' => 'fail', 500]);
+        }
+
         return response()->json(['message' => 'success'], 200);
 //        return $this->successResponse('가입하신 메일 계정으로 가입 확인 메일을 보내드렸습니다. 가입 확인 하시고 로그인해 주세요.');
     }
@@ -52,23 +65,23 @@ class UsersController extends Controller
         return redirect('/');
     }
 
-    public function confirm($code)
-    {
-        $user = \App\User::whereConfirmCode($code)->first();
-
-        if (!$user) {
-            flash('URL이 정확하지 않습니다.');
-            return redirect('/');
-        }
-        $user->activated = 1;
-        $user->confirm_code = null;
-        $user->save();
-
-        auth()->login($user);
-        flash(auth()->user()->name . '님 환영합니다. 가입 확인되었습니다.');
-
-        return redirect('/');
-    }
+//     public function confirm($code)
+//     {
+//         $user = \App\User::whereConfirmCode($code)->first();
+//
+//         if (!$user) {
+//             flash('URL이 정확하지 않습니다.');
+//             return redirect('/');
+//         }
+//         $user->activated = 1;
+//         $user->confirm_code = null;
+//         $user->save();
+//
+//         auth()->login($user);
+//         flash(auth()->user()->name . '님 환영합니다. 가입 확인되었습니다.');
+//
+//         return redirect('/');
+//     }
 
 
 }
