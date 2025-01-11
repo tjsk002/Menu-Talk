090ecc60b8cf0d5bbf64888ab7c1312e827e99ee
tjsk002 <tjsk002@naver.com>
2025-01-11 23:27:01 +0900

    user model edit

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,8 +12,13 @@ class User extends Model
     protected $table = 'users';
     protected $fillable = [
         'name',
+        'phone_number',
         'email',
-        'password'
+        'password',
+        'business_id',
+        'status',
+        'company_name',
+        'company_number',
     ];
 
     public function getName(): string
@@ -22,7 +27,32 @@ class User extends Model
     }
 
     public function getEmail(): string
-        {
-            return $this->email;
-        }
+    {
+        return $this->email;
+    }
+
+    public function getPhoneNumber(): string
+    {
+        return $this->phone_number;
+    }
+
+    public function getBusinessId (): string
+    {
+        return $this->business_id;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    public function getCompanyName(): string
+    {
+        return $this->company_name;
+    }
+
+    public function getCompanyNumber(): string
+    {
+        return $this->company_number;
+    }
 }
