06e8647eb251c364119b10e2678e271419aafdec
tjsk002 <tjsk002@naver.com>
2025-01-16 16:50:53 +0900

    user model 수정

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class User extends Model
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory;
 
