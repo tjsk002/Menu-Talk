21fb61f672905585a41f37991b52bcb60fa40052
tjsk002 <tjsk002@naver.com>
2025-01-20 17:28:58 +0900

    middleware auth add

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,7 +52,6 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        // auth 별칭은 여기서 나온거다
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
